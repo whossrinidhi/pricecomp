4143dd8f23511acf046af32acafe9f4b8747ba92
srinidhinitya <96458682+srinidhinitya@users.noreply.github.com>
2023-04-20 07:54:44 +0530

    upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("button");
+const box = document.getElementById("inputel");
+btn.addEventListener("click", function () {
+  let product = box.value;
+  console.log(product);
+});
